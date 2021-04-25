2eeab271059d8f637c5aad811880be8330037135
JM-AK <komlevalexandr87@gmail.com>
2021-04-25 22:47:28 +0300

    [b1.02] replace repository of Contact from Jpa TO Sql2O

--- src/main/java/ru/dv/mailhelper/repositories/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.dv.mailhelper.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.dv.mailhelper.entities.Contact;
-
-@Repository
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
